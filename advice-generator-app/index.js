3ec11ed21a1a5bf3852d88c3a904f0a9794a0fa2
Harvey Yerik <yerik05vh@gmail.com>
2022-05-29 17:55:29 -0500

    advice generator app

---
+++ advice-generator-app/index.js
@@ -0,0 +1,27 @@
+const id = document.getElementById("id")
+const advice = document.getElementById("advice")
+const btn = document.getElementById("btn-random-click")
+
+
+async function getData(){
+    try {
+        let res = await fetch("https://api.adviceslip.com/advice")
+        let json = await res.json()
+        let data = json.slip
+        localStorage.setItem("id",`${data.id}`)
+        localStorage.setItem("advice",`${data.advice}`)
+        location.reload()
+    } catch (err) {
+        advice.innerHTML=`Ocurrio un error, Vuelve a intentarlo`
+    }
+   
+}
+btn.addEventListener('click',e=>{
+    getData()
+})
+const theId = localStorage.getItem("id")
+const theAdvice = localStorage.getItem('advice')   
+document.addEventListener("DOMContentLoaded",e=>{
+    id.innerHTML=`ADVICE <i>#${theId}</i>`
+    advice.innerHTML = `${theAdvice}`
+})
